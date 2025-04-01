b22444edc38e47e0b1eb2c1e51772e85f67370e5
ArthurDalpiazTessele <arthurdtessele@gmail.com>
2025-04-01 20:42:24 -0300

    test

--- chat_Flutter_Com_WebSocket-main/lib/screens/chat_page.dart
+++ chat_Flutter_Com_WebSocket-main/lib/screens/chat_page.dart
@@ -5,6 +5,7 @@ import 'package:flutter_chat_ui/flutter_chat_ui.dart';
 import 'package:flutter_chat_types/flutter_chat_types.dart' as types;
 import 'package:web_socket_client/web_socket_client.dart';
 import 'package:mime/mime.dart';
+import 'package:open_filex/open_filex.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:image_picker/image_picker.dart';
 
